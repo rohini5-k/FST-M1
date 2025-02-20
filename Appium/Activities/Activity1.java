32d03aa7551eb3acb88b1f298086508283947447
rohini5-k <63206554+rohini5-k@users.noreply.github.com>
2025-02-20 12:38:30 +0530

    Add files via upload

---
+++ Appium/Activities/Activity1.java
@@ -0,0 +1,50 @@
+package activities;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+
+public class Activity1 {
+
+    AndroidDriver driver;
+    
+    @BeforeClass
+    public void setUp() throws MalformedURLException, URISyntaxException {
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setPlatformName("android");
+        options.setAutomationName("UiAutomator2");
+        options.setAppPackage("com.android.calculator2");
+        options.setAppActivity(".Calculator");
+        options.noReset();
+
+        URL serverURL = new URI("http://localhost:4723").toURL();
+        driver = new AndroidDriver(serverURL, options);
+    }
+
+    @Test
+    public void multiplyTest() {
+        driver.findElement(AppiumBy.id("digit_5")).click();
+        driver.findElement(AppiumBy.accessibilityId("×")).click();
+        driver.findElement(AppiumBy.id("digit_8")).click();
+        driver.findElement(AppiumBy.accessibilityId("equals")).click();
+
+        String result = driver.findElement(AppiumBy.id("result")).getText();
+        Assert.assertEquals(result, "40");
+    }
+
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+}
